3d9e3ab38b26c0e83a3845cecd4ee2d290923ee7
Manjunath-5717 <manjunathragati704@gmail.com>
2026-02-25 18:02:23 +0530

    Fixed project structure

---
+++ Builtop.java
@@ -0,0 +1,21 @@
+public class Builtop{
+
+public static void main(String[] args){
+
+double d=-20.20;
+System.out.println("math.abs "+Math.abs(d));
+double e=Math.abs(d);
+System.out.println("math.ciel "+Math.ceil(e));
+System.out.println("math.floor "+Math.floor(e));
+System.out.println("math.max "+Math.max(2,3));
+System.out.println("math.min "+Math.min(2,3));
+
+System.out.println("math.random 10 to 100"+ Math.random());
+
+int random = (int)(10 + Math.random() * 91);
+System.out.println("math.random 10 to 100 "+ Math.random());
+
+}
+
+}
+
